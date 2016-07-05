05b78f45eb897d0df68e4f9ef1d395dd2e92fed3
Brendan Metcalfe <bgmetcalfe@gmail.com>
2016-07-05 17:58:23 +0000

    completed first question

--- answers.js
+++ answers.js
@@ -1,1 +1,22 @@
-yooo
+// Write a function that takes a string 
+// and returns the first character of the string. 
+//Test your function on a few inputs, including the empty string.
+
+function stringCharReturner (myString) {
+        var firstCharOfString = myString[0];
+        return firstCharOfString;
+}
+
+console.log(stringCharReturner("money"));
+
+
+//Write a function that takes a string 
+//and returns the last character of a string. 
+// Test your function on a few inputs, including the empty string.
+
+function stringCharReturner (myString) {
+        var lastCharOfString = myString.length;
+        return lastCharOfString;
+}
+
+console.log(stringCharReturner("money"));
